8375af09555994c71d3f39fcecec721a5434de4d
lalitha1312 <lalithaag04@gmail.com>
2025-05-10 21:40:35 +0530

    Delete myprojectback/review directory

--- myprojectback/review/src/main/java/com/book/review/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.book.review.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.book.review.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
--- myprojectback/review/src/main/java/com/book/review/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.book.review.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.book.review.model.Review;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
